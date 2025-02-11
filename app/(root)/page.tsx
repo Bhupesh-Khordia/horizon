17e0cbb9d59964fc818ca41aca606c520d2865b3
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-02-11 10:22:34 +0530

    Basic Home Page UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const loggedIn = { firstName: "Bhupesh" };
 
-export default Home
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1225.65}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
