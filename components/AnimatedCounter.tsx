87de8f26e161482b008f45e8b2e610b1c40f86ae
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-02-19 22:54:37 +0530

    Displays Real Bank Data on Home Page

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -9,7 +9,7 @@ const AnimatedCounter = ({ amount }: { amount: number }) => {
         duration={0.9}
         decimals={2}
         decimal="."
-        prefix="₹"
+        prefix="$"
         end={amount}
       />
     </div>
